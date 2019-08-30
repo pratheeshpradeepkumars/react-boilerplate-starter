601666320d31ea2b34e8b2c9a4e863ec68d4c29b
pratheesh.p <pratheesh.p@appviewx.com>
2019-08-30 16:53:49 +0530

    Refactored with  optimized approach

--- webpack.common.js
+++ webpack.common.js
@@ -1,47 +1,27 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebPackPlugin  = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin  = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         main: './src/index.js'
     },
-    output: {
-        filename: '[name].[hash].js',
-        path: path.resolve('./dist')
-    },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: [/node_modules/], // exclude: [path.resolve(__dirname, 'node_modules')]
+                exclude: [/node_modules/],
                 use: [{ loader: 'babel-loader' }]
             },
             {
-                test: /\.less$/,
-                use: [{
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            strictMath: true,
-                            noIeCompat: true,
-                        }
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
                     }
-                ]
+                }
             }
         ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: 'index.html'
-        }),
-        new CleanWebpackPlugin({
-            cleanAfterEveryBuildPatterns: ['dist']
-        })
-    ]
+    }
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,7 @@
 const merge = require('webpack-merge');
 const common = require("./webpack.common.js");
+const HtmlWebpackPlugin  = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
     mode: 'development',
@@ -7,5 +9,25 @@ module.exports = merge(common, {
         host: 'localhost',
         port: 3000,
         open: true
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.less$/,
+            use: [
+                "style-loader",
+                "css-loader",
+                "less-loader"
+            ]
+          }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['dist']
+        })
+    ]
 });
